1af1a1f8715a76abecb895d8ec0131aa42cf7b3d
KimTrien <kimtrien@gmail.com>
2023-11-07 01:24:17 +0700

    add login layout class

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,6 +2,8 @@
 
 @section('meta_title', __('customer::customer.login.meta_title'))
 
+@section('body-class', 'login-layout')
+
 @section('content')
     @includeFirst(['auth.login', 'customer::auth.login-content'])
 @endsection
