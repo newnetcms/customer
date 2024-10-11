00cb47785ec95e4d548e8c91ca1b543da59b0e44
KimTrien <kimtrien@gmail.com>
2024-10-11 14:50:49 +0700

    fix verify email from guest

--- src/Http/Controllers/Auth/VerificationController.php
+++ src/Http/Controllers/Auth/VerificationController.php
@@ -2,9 +2,14 @@
 
 namespace Newnet\Customer\Http\Controllers\Auth;
 
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\VerifiesEmails;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+use Newnet\Customer\Models\Customer;
 
 class VerificationController extends Controller
 {
@@ -17,7 +22,7 @@ class VerificationController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:customer');
+        $this->middleware('auth:customer')->except('verify');
         $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
     }
@@ -45,5 +50,28 @@ class VerificationController extends Controller
         $config = config('cms.customer.redirect_after_login', route('customer.web.customer.profile'));
 
         return is_callable($config) ? $config() : $config;
+    }
+
+    public function verify(Request $request)
+    {
+        $user = Customer::find($request->route('id'));
+
+        if (!hash_equals((string) $request->route('hash'), sha1($user->getEmailForVerification()))) {
+            throw new AuthorizationException;
+        }
+
+        if ($user->markEmailAsVerified()) {
+            event(new Verified($user));
+        }
+
+        Auth::guard('customer')->login($user);
+
+        if ($response = $this->verified($request)) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+            ? new JsonResponse([], 204)
+            : redirect($this->redirectPath())->with('verified', true);
     }
 }
